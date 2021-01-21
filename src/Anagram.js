d635e01d8f7eab44cd0d5d083cfef518c32fad1a
cwfietz <cwfietz@calcna.ab.ca>
2021-01-20 21:57:31 -0700

    Add first method and a test that fails.

---
+++ src/Anagram.js
@@ -0,0 +1,5 @@
+const getBlueBerry = () => {
+    return false;
+};
+
+export { getBlueBerry };
---
+++ src/__test__/Anagram.test.js
@@ -0,0 +1,5 @@
+import { getBlueBerry } from '../../src/Anagram.js';
+
+test('finds that getBlueBerry is true', () => {
+    expect(getBlueBerry()).toBe(true); 
+});
