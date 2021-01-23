389b02319e6c2537612b80f4b580d59b613e2873
cwfietz <cwfietz@calcna.ab.ca>
2021-01-22 22:15:35 -0700

    Add getLettersProfile(), alphaVal()

--- src/Anagram.js
+++ src/Anagram.js
@@ -7,4 +7,18 @@ const getEmptyLetterProfile = () => {
     return [...templateEmptyProfile];
 };
 
-export { isAnagram, getEmptyLetterProfile };
+const alphaVal = (inputString) => inputString.toLowerCase().charCodeAt(0) - 97;
+
+const getLettersProfile = (word) => {
+    let letterProfile = getEmptyLetterProfile();
+    let letterNumber = 0;
+    for (let letter of word) {
+        letterNumber = alphaVal(letter);
+        if(0 <= letterNumber && letterNumber <= 26) {
+            letterProfile[letterNumber]++;
+        }
+    }
+    return letterProfile;
+}
+
+export { isAnagram, getEmptyLetterProfile, getLettersProfile };
--- src/__test__/Anagram.test.js
+++ src/__test__/Anagram.test.js
@@ -1,4 +1,4 @@
-import { isAnagram, getEmptyLetterProfile } from '../../src/Anagram.js';
+import { isAnagram, getEmptyLetterProfile, getLettersProfile } from '../../src/Anagram.js';
 
 test('finds that king and kins are not anagrams', () => {
     expect(isAnagram('king','kins')).toBe(false); 
@@ -6,4 +6,8 @@ test('finds that king and kins are not anagrams', () => {
 
 test('returns empty letterProfile', () => {
     expect(getEmptyLetterProfile()).toEqual([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+});
+
+test('letters profile for kinship is ()', () => {
+    expect(getLettersProfile('kinship')).toEqual([0, 0, 0, 0, 0, 0, 0, 1, 2, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0]);
 });
